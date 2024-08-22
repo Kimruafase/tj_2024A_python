079c86271345e0a41b036ab1ecfd8d642c6fcee3
kimruafase <ppooppplus33@gmail.com>
2024-08-22 18:09:07 +0900

    Python day07

--- src/day07/task9/Main.py
+++ src/day07/task9/Main.py
@@ -13,25 +13,25 @@ from Region import Region
 list = []
 def load() :
     try :
-        f = open("인천광역시_부평구_인구현황.csv", "r")
-        next(f)
-        pop = f.read()
-        lines = pop.split("\n")
-        for i in lines :
-            if i :
-                cols = i.split(",")
-                popluation = Region(cols[0], cols[1], cols[2], cols[3], cols[4])
-                list.append(popluation)
-        f.close()
-        return list
+        f = open("인천광역시_부평구_인구현황.csv", "r") # 파일 열기
+        next(f)                                     # 파일 첫번쨰 줄 스킵
+        pop = f.read()                              # 파일 전체 읽여서 저장
+        lines = pop.split("\n")                     # 줄바꿈 처리한 기준으로 자름
+        for i in lines :                            # 반복문
+            if i :                                  # 반복 변수가 존재한다면
+                cols = i.split(",")                 # "," 기준으로 잘라서 변수에 저장
+                popluation = Region(cols[0], cols[1], cols[2], cols[3], cols[4])    # Region 클래스 생성자로 인수 5개 넣어서 생성
+                list.append(popluation)             # 리스트에 대입
+        f.close()                                   # 파일 닫음
+        return list                                 # 리스트 반환
     except Exception :
         return list
 
 
 if __name__ == "__main__" :
     print("--start--")
-    list = load()
-    for i in list :
-        ratio = i.ratio(i.people, i.man)
-        print(f"{i.region:5s}, {i.people:5d}, {i.man:5d}, {i.woman:5d}, {i.home:5d}, {ratio}% {100-ratio}%")
+    list = load()               # load() 메소드 호출 후 리스트에 대입
+    for i in list :             # 반복문
+        ratio = i.ratio(i.people, i.man)    # 반복변수의 ratio 메소드 호출해서 반복변수의 people 객체와 man 객체 매개변수로 보내고 계산값 반환받음
+        print(f"{i.region:5s}, {i.people:5d}, {i.man:5d}, {i.woman:5d}, {i.home:5d}, {ratio}% {100-ratio}%")    # 객체와 ratio 메소드 계산값 출력
 
--- src/day07/task9/Region.py
+++ src/day07/task9/Region.py
@@ -1,7 +1,7 @@
 # day07 > task9 > Region.py
 
 class Region :
-    def __init__(self,region,people, man, woman, home):
+    def __init__(self,region,people, man, woman, home): # 생성자
         self.region = region
         self.people = int(people)
         self.man = int(man)
@@ -9,4 +9,4 @@ class Region :
         self.home = int(home)
 
     def ratio(self, people, man) :
-        return int(100*(self.man / self.people))
+        return int(100*(self.man / self.people))    # 전체 인구수에 따른 남자 인구수 비율 계산
