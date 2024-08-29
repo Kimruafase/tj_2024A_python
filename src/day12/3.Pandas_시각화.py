8753511289486666ef06ccad8a49ac144d3f02a8
kimruafase <ppooppplus33@gmail.com>
2024-08-29 12:13:56 +0900

    Python day12

--- src/day12/3.Pandas_시각화.py
+++ src/day12/3.Pandas_시각화.py
@@ -3,23 +3,37 @@ import pandas as pd
 import matplotlib as plt
 import matplotlib.pyplot as plt
 
+# [1] pandas 이용해서 csv 파일 생성해보기
+# 1. 리스트 변수 a ~ f 까지 선언해서 값 저장
 a = [500, 450, 520, 610]
 b = [690, 700, 820, 900]
 c = [1100, 1030, 1200, 1380]
 d = [1500, 1650, 1700, 1850]
 e = [1990, 2020, 2300, 2420]
 f = [1020, 1600, 2200, 2550]
+
+# 인덱스 에 넣을 제목 리스트 생성
 index1 = ["2015년", "2016년", "2017년", "2018년", "2019년", "2020년"]
+
+# 각 열에 맞는 제목 리스트 생성
+columns1 = ["1분기", "2분기", "3분기", "4분기"]
+
+# 2차원 배열 DataFrame 사용하기 위해서 list1 리스트 생성해서 a ~ f 까지 저장
 list1 = [a,b,c,d,e,f]
 
-list2 = pd.DataFrame(list1, columns = ["1분기", "2분기", "3분기", "4분기"], index=index1)
+# pandas 의 DataFrame 이용해서 행과 열에 제목 정해주고 2차원 배열 생성
+list2 = pd.DataFrame(list1, columns = columns1, index = index1)
 
+# 생성한 2차원 배열을 csv 파일로 변환
 list2.to_csv("sales.csv", encoding="utf-8", mode="w", index = True)
 
-# 1) 차트에 표시할 샘플 데이터 준비
+# [2] matplotlib 이용해서 차트 만들기
+# 1. 차트에 표시할 샘플 데이터 준비 (위에 되어 있음)
+
+# 2. x축의 길이를 샘플 데이터의 길이만큼으로 설정
 x = range(len(a))
 
-# 2) 라인 plot(선 차트)에 x 축과 y 축 지정하여 라인 플롯 형성
+# 3. 라인 plot(선 차트)에 x 축과 y 축 지정하여 라인 플롯 형성
 plt.plot(x, a, color="green")
 plt.plot(x, b, color="red")
 plt.plot(x, c, color="blue")
@@ -27,24 +41,21 @@ plt.plot(x, d, color="orange")
 plt.plot(x, e, color="black")
 plt.plot(x, f, color="yellow")
 
-# 3) 차트 제목 넣기
+# 4. 차트 제목 넣기
 plt.title("2015 - 2020 Quarterly Sales")
 
-# 4) x축 레이블(제목) 설정
+# 5. x축 레이블(제목) 설정
 plt.xlabel("Quarters")
 
-# 5) y축 레이블(제목) 설정
+# 6. y축 레이블(제목) 설정
 plt.ylabel("sales")
 
-# 6) 눈금 이름 리스트 설정, x축
+# 7. 눈금 이름 리스트 설정, x축
 xLabel = ["first", "second", "third", "fourth"]
 plt.xticks(x, xLabel, fontsize = 10)
 
-# 7) 눈금 이름 리스트 설정, y축
-yLabel = ["500", "1000", "1500", "2000", "2500"]
-
-# 범례(막대 구문 이름 표시)
+# 8. 범례(막대 구문 이름 표시)
 plt.legend(["2015", "2016", "2017", "2018", "2019", "2020"])
 
-# 6) 차트 실행
+# 9. 차트 실행
 plt.show()
